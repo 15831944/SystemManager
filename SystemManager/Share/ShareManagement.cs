8bcc9810a6e73fe24552a62dca4c51de17717bd9
ststeiger <quandary82@hailmail.net>
2016-03-10 18:24:22 +0100

    Added Share Management

---
+++ SystemManager/Share/ShareManagement.cs
@@ -0,0 +1,86 @@
+﻿
+using Trinet.Networking;
+
+
+namespace SimpleTests
+{
+
+
+    class ShareManagement
+    {
+
+        /*
+        [System.Runtime.InteropServices.DllImport("Netapi32.dll", CharSet = System.Runtime.InteropServices.CharSet.Unicode)]
+        private static extern int NetShareEnum(
+             System.Text.StringBuilder ServerName,
+             int level,
+             ref System.IntPtr bufPtr,
+             uint prefmaxlen,
+             ref int entriesread,
+             ref int totalentries,
+             ref int resume_handle
+             );
+        */
+
+
+
+        public static System.Collections.Generic.List<Share> ListShares()
+        {
+            System.Console.WriteLine(System.Environment.NewLine + "Shares on local computer:");
+            System.Collections.Generic.List<Share> ls = new System.Collections.Generic.List<Share>();
+
+            ShareCollection shi = ShareCollection.LocalShares;
+            if (shi == null)
+                return ls;
+
+            foreach (Share si in shi)
+            {
+                System.Console.WriteLine("{0}: {1} [{2}]", si.ShareType, si, si.Path);
+                ls.Add(si);
+            } // Next si 
+
+            return ls;
+        } // End Function ListShares 
+
+
+        public static void ListShareInfo()
+        {
+            System.Console.WriteLine(System.Environment.NewLine + "Shares on local computer:");
+            ShareCollection shi = ShareCollection.LocalShares;
+            if (shi != null)
+            {
+                foreach (Share si in shi)
+                {
+                    System.Console.WriteLine("{0}: {1} [{2}]", si.ShareType, si, si.Path);
+
+                    // If this is a file-system share, try to
+                    // list the first five subfolders.
+                    // NB: If the share is on a removable device,
+                    // you could get "Not ready" or "Access denied"
+                    // exceptions.
+
+                    if (si.IsFileSystem)
+                    {
+                        try
+                        {
+                            System.IO.DirectoryInfo d = si.Root;
+                            System.IO.DirectoryInfo[] Flds = d.GetDirectories();
+                            for (int i = 0; i < Flds.Length && i < 5; i++)
+                                System.Console.WriteLine("\t{0} - {1}", i, Flds[i].FullName);
+
+                            System.Console.WriteLine();
+                        }
+                        catch (System.Exception ex)
+                        {
+                            System.Console.WriteLine("\tError listing {0}:\n\t{1}\n", si, ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+                System.Console.WriteLine("Unable to enumerate the local shares.");
+        }
+    }
+
+
+}
